d991c49dadc6cf68ea366746f1cd42d6a959f706
Jonathan Murmu <jonathanmurmu@gmail.com>
2019-02-13 22:01:11 +0530

    basic detail page

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from todo import views
 
 urlpatterns = [
     path('', views.TodoList.as_view(), name='todo-list'),
+    path('<int:pk>/', views.TodoDetailView.as_view(), name='todo-detail'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from todo.models import Todo
 
@@ -12,4 +12,13 @@ class TodoList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Todo Home'
+        return context
+
+class TodoDetailView(DetailView):
+
+    model = Todo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Todo'
         return context
