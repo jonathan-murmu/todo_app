03ebb99882ad991f06f888c9da84460a85cd8382
Jonathan Murmu <jonathanmurmu@gmail.com>
2019-02-13 21:23:45 +0530

    models and admin

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """Model for todo app."""
+    PENDING = 'PENDING'
+    IN_PROGRESS = 'IN_PROGRESS'
+    COMPLETED = 'COMPLETED'
+    STATUS_CHOICES = (
+        (1, PENDING),
+        (2, IN_PROGRESS),
+        (3, COMPLETED),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    task_time = models.DateTimeField(null=True, blank=True)
+    status = models.PositiveSmallIntegerField(default=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
