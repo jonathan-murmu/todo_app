9164610ac6ddb7940a1c36ca3928e3e727ed658a
Jonathan Murmu <jonathanmurmu@gmail.com>
2019-02-15 22:29:21 +0530

    search and filter in admin section

--- todo/admin.py
+++ todo/admin.py
@@ -6,6 +6,10 @@ from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ("title", "description", "task_time", "status", "created_at", "modified_at", "is_active")
+    search_fields = (
+        'title', 'description'
+    )
+    list_filter = ('status', 'is_active')
     actions = ["export_as_csv"]
 
 
